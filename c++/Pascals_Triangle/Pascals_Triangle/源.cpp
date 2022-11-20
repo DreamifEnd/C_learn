d6d10a133531b378266975cf1225d4e3609dc87f
DreamEnd <2201325624@qq.com>
2022-11-20 16:32:45 +0800

    11_20

---
+++ c++/Pascals_Triangle/Pascals_Triangle/源.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+#define N 7
+int main()
+{
+	int arr[N][N] = { 0 };
+	arr[0][0] = 1;
+	int i = 0, j = 0;
+	for (i = 0; i < N; i++)
+		for (j = 0; j <= i; j++)
+		{
+			if (i == 1||j==0)
+				arr[i][j] = 1;
+			else
+				arr[i][j] = arr[i - 1][j] + arr[i - 1][j - 1];
+		}
+	int n = 0;
+	j=0;
+	for (i = 0; i < N; i++)
+	{
+		for (n = 0; n <= N - j - 1; n++)
+			cout << "    ";
+		for (j = 0; j <= i; j++)
+			cout << arr[i][j] << "        ";
+		cout << endl;
+	}
+	return 0;
+}
