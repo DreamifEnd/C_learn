9be99b09b202cf139aadb8ccb93092b31c23ae76
DreamEnd <2201325624@qq.com>
2022-12-10 20:11:53 +0800

    12_10

--- c++/class_1/class_1/test.cpp
+++ c++/class_1/class_1/test.cpp
@@ -1,9 +1,32 @@
 #include<iostream>
+#include<string>
 using namespace std;
+class student {
+private:
+	string Name;
+	int Number;
+public:
+	void input(string name, int number)
+	{
+		Name = name;
+		Number = number;
+	}
+	void output(string name,int number)
+	{
+		name = Name;
+		number = Number;
+	}
+
+
+};
 int main()
 {
-	class juxing {
-		
-	};
+	string name;
+	int number = 0;
+	cin >> name >> number;
+	student s1;
+	s1.input(name, number);
+	s1.output(name, number);
+	cout << name <<'\t' << number << endl;
 	return 0;
 }
