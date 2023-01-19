26491e27eb98db28e2a0c6df79bbf6fd92f6cabc
盛北城 <2201325624@qq.com>
2023-01-19 22:17:51 +0800

    1_19

---
+++ c++/Mode/test.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<cstdio>
+using namespace std;
+int main()
+{
+	
+	return 0;
+} 
+int main1()
+{
+	char s;
+	while(1)
+	{
+		s = getchar();
+		if(s == EOF)
+		{
+			break;
+		}
+		//cout<<s;
+		putchar(s);
+	}
+	return 0;
+}
